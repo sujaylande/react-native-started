9fe2c8554f0b825af508e90cd579a62483cc4637
Sujay <122771820+sujaylande@users.noreply.github.com>
2025-05-27 15:18:53 +0530

    hello react-native

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+
+
+const Index = () => {
+  return (
+    <View style={{backgroundColor: "pink", flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{fontSize: 30, fontWeight: "700"}}>Hello World!</Text>
+    </View>
+  )
+}
+
+export default Index
